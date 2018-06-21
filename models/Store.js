6c8a0f4dd366f4a1ca6ff68541db9fec1f40d7c5
Swarleys <elessar29@gmail.com>
2018-06-21 19:41:18 +0200

    Custom queries and hooks

--- models/Store.js
+++ models/Store.js
@@ -35,14 +35,29 @@ const storeSchema = new mongoose.Schema({
   photo: String,
 });
 
-storeSchema.pre('save', function(next){
+storeSchema.pre('save', async function(next){
   if (!this.isModified('name')) {
     next(); //skip it
     return; // stop this function from running
   }
   this.slug = slug(this.name);
+  // find other stores that have the same slug
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+
+  if(storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`
+  }
+
   next();
-  // TODO make more resiliant so slugs are unique
-})
+});
+
+storeSchema.statics.getTagsList = function() {
+  return this.aggregate([
+    { $unwind: '$tags'}, //crea una "entrada" por tap, si una tienda tiene 3 tags te lo separa en 3
+    { $group: { _id: '$tags', count: { $sum: 1 } } }, //Agrupa por tags
+    { $sort: { count: -1 } }, // ordena de mas a menos
+  ]);
+}
 
 module.exports = mongoose.model('Store', storeSchema);
